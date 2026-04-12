ba48b149ae00ce4304f564b00de65765659b3115
Hanna <hnnzpa@email.com>
2026-04-12 16:38:46 +0200

    Fet i funciona 12-Bany Unisex

--- 12-Bany_unisex/src/BanyUnisex.java
+++ 12-Bany_unisex/src/BanyUnisex.java
@@ -7,50 +7,137 @@ public class BanyUnisex {
     private final Object BANY_AMB_HOMES = new Object();
     private final Object BANY_AMB_DONES = new Object();
 
-
     private Object estatActual = BANY_BUIT;
     private int ocupants = 0;
     private final int CAPACITAT_MAX = 3;
 
+    public BanyUnisex() {
+    }
+
     private Semaphore capacitat = new Semaphore(CAPACITAT_MAX, true);
 
-    private ReentrantLock lockEstat = new ReentrantLock();
+    private ReentrantLock lockEstat = new ReentrantLock(true);
 
-    public void entraHome() throws Exception{
-        synchronized(BANY_AMB_HOMES) {
-            while(estatActual == BANY_AMB_DONES){
-                BANY_AMB_HOMES.wait();
+    public String getEstat(){
+        String estat;
+        if(estatActual == BANY_AMB_DONES){
+            estat = "Dones";
+        }else if (estatActual == BANY_AMB_HOMES){
+            estat = "Homes";
+        }else {
+            estat = "Buit";
+        }
+        return estat;
+    }
+
+    public void entraHome() {
+        while (true) {
+            lockEstat.lock();
+
+            try {
+                if (estatActual != BANY_AMB_DONES) {
+                    if (estatActual == BANY_BUIT) {
+                        estatActual = BANY_AMB_HOMES;
+                    }
+
+                } else {
+                    continue;
+                }
+            } finally {
+                lockEstat.unlock();
             }
-            if (estatActual != BANY_AMB_HOMES){
-                estatActual = BANY_AMB_HOMES;}
-            if(capacitat.tryAcquire()){
-                try{
-                    lockEstat.lock();
-                    ocupants++;
-                }finally{
+
+            if (capacitat.tryAcquire()) {
+                lockEstat.lock();
+                try {
+                    ocupants++; // suma para tener la capacidad para los mensajes.
+                    System.out.printf("Home entra al bany. Ocupants: %d, Estat: %s%n", ocupants, getEstat());
+                    break;
+                } finally {
                     lockEstat.unlock();
                 }
             }
+
+        }
+
+    }
+
+    public void surtHome() {
+        lockEstat.lock();
+        try {
+            ocupants--;
+            capacitat.release();
+
+            if (ocupants == 0) {
+                estatActual = BANY_BUIT;
+            }
+            System.out.printf("Home surt del bany. Ocupants: %d, Estat: %s%n", ocupants, getEstat());
+
+        } finally {
+            lockEstat.unlock();
         }
     }
 
-    public void entraDona() throws Exception{
-        synchronized(BANY_AMB_DONES) {
-            while(estatActual == BANY_AMB_HOMES){
-                BANY_AMB_DONES.wait();
+    public void entraDona() {
+        while (true) {
+            lockEstat.lock();
+
+            try {
+                if (estatActual != BANY_AMB_HOMES) {
+                    if (estatActual == BANY_BUIT) {
+                        estatActual = BANY_AMB_DONES;
+                    }
+
+                } else {
+                    continue;
+                }
+            } finally {
+                lockEstat.unlock();
             }
-            if (estatActual != BANY_AMB_DONES){
-                estatActual = BANY_AMB_DONES;}{
-            if(capacitat.tryAcquire()){
-                try{ 
-                    lockEstat.lock();
-                    ocupants++;
-                }finally{
+
+            if (capacitat.tryAcquire()) {
+                lockEstat.lock();
+                try {
+                    ocupants++; // suma para tener la capacidad para los mensajes.
+                    System.out.printf("Dona entra al bany. Ocupants: %d, Estat: %s%n", ocupants, getEstat());
+                    break;
+                } finally {
                     lockEstat.unlock();
                 }
             }
+
         }
+
     }
+
+    public void surtDona() {
+        lockEstat.lock();
+        try {
+            ocupants--;
+            capacitat.release();
+
+            if (ocupants == 0) {
+                estatActual = BANY_BUIT;
+            }
+            System.out.printf("Dona surt del bany. Ocupants: %d, Estat: %s%n", ocupants, getEstat());
+
+        } finally {
+            lockEstat.unlock();
+        }
+
+    }
+
     
+
+    public static void main(String[] args) {
+
+        BanyUnisex bany = new BanyUnisex();
+        for (int i = 0; i < 5; i++) {
+            Home h = new Home("Home-" + i, bany);
+            h.start();
+            Dona d = new Dona("Dona-" + i, bany);
+            d.start();
+        }
+    }
 
 }
--- 12-Bany_unisex/src/Home.java
+++ 12-Bany_unisex/src/Home.java
@@ -1,17 +1,27 @@
+import java.util.Random;
+
 public class Home extends Thread{
     private BanyUnisex bany;
+    private Random rand = new Random();
 
     public Home(String name, BanyUnisex bany) {
         super(name);
         this.bany = bany;
     }
 
-
-    public entrar(){
-        
+    public void utilitzaLavabo(){
+        int sec = rand.nextInt(2000, 3000);
+        try{
+            sleep(sec);
+            System.out.printf("%s ha acabat d'usar el bany%n", this.getName());
+        }catch (Exception e){ e.getMessage();}
     }
 
 
-
-
+    public void run(){
+        System.out.printf("%s vol entrar al bany%n", this.getName());
+        bany.entraHome();
+        utilitzaLavabo();
+        bany.surtHome();
+    }
 }
